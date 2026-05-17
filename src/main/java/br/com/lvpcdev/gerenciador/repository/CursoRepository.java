a483efa744a47df0c6499db267785aed4f14809d
Lucas Vicente <lucasvpc47@gmail.com>
2026-05-17 18:55:21 -0300

    Adicionando filtragem por ativo e inativo no aluno e curso

--- src/main/java/br/com/lvpcdev/gerenciador/repository/CursoRepository.java
+++ src/main/java/br/com/lvpcdev/gerenciador/repository/CursoRepository.java
@@ -4,6 +4,12 @@ import br.com.lvpcdev.gerenciador.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    List<Curso> findAllByAtivoTrue();
+
+    List<Curso> findAllByAtivoFalse();
 }
