c1b469254feaaf96bf95518c0763d177f711fc1b
Lucas Vicente <lucasvpc47@gmail.com>
2026-06-01 16:35:57 -0300

    Fazendo ultimas alterações para colocar no ar

---
+++ src/main/java/br/com/lvpcdev/gerenciador/GerenciadorAlunosApplication.java
@@ -0,0 +1,11 @@
+package br.com.lvpcdev.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorAlunosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorAlunosApplication.class, args);
+    }
+}
--- src/main/java/br/com/lvpcdev/gerenciador/model/Aluno.java
+++ src/main/java/br/com/lvpcdev/gerenciador/model/Aluno.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
 public class Aluno {
 
